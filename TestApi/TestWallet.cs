cb5f0f6fbc5edb70fff9265881e8d610c069dd07
Jose Solano R <josesolano@outlook.com>
2019-09-02 21:51:34 -0400

    Creando los metodos de pruebas unitarias

---
+++ TestApi/TestWallet.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestApi
+{
+    [TestClass]
+    public class TestWallet
+    {
+        [TestMethod]
+        public void ShouldShowYourCurrentBalance()
+        {
+
+        }
+        [TestMethod]
+        public void ShouldNotShowNegativeBalance()
+        {
+
+        }
+        [TestMethod]
+        public void ShouldAddIncomesToYourBalance()
+        {
+
+        }
+        [TestMethod]
+        public void ShouldWithdrawFundsFromYourBalance()
+        {
+
+        }
+        [TestMethod]
+        public void ShouldNotWithdrawNegativesFundsFromYourBalance()
+        {
+
+        }
+    }
+}
