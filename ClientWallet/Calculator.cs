8da799f8968c3eec71ac6358e28c2681ef96ec31
Jose Solano R <josesolano@outlook.com>
2019-09-04 23:21:58 -0400

    Implementando Specflow

---
+++ ClientWallet/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientWallet
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+    }
+}
