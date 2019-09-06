0114bb41828fa4645b44cfc5b8c70f7bc87faf03
Jose Solano R <josesolano@outlook.com>
2019-09-06 02:21:15 -0400

    Pruebas de UI finalizadas, investigar mejor llamado a ellas

---
+++ TestClient/Specs/UIAddDepositSteps.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using System.Reflection;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace TestClient.Specs
+{
+    [Binding]
+    public class UIAddDepositSteps
+    {
+        IWebDriver currentDriver;
+        decimal data;
+        string balanceData, newBalanceData;
+
+        [Given(@"El monto de (.*) bolivianos")]
+        public void GivenElMontoDeBolivianos(int number)
+        {
+            data = number;
+        }
+
+        [When(@"Navego a la pagina principal")]
+        public void WhenNavegoALaPaginaPrincipal()
+        {
+            currentDriver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
+            {
+                Url = "http://localhost:20001"
+            };
+        }
+
+        [When(@"Hago click en el boton Depositar")]
+        public void WhenHagoClickEnElBotonDepositar()
+        {
+            currentDriver.FindElement(By.Name("Depositar")).Click();
+        }
+
+        [When(@"Lleno el campo del monto a depositar")]
+        public void WhenLlenoElCampoDelMontoADepositar()
+        {
+            System.Threading.Thread.Sleep(2000);
+            balanceData = currentDriver.FindElement(By.Id("dataBalance")).GetAttribute("innerHTML");
+            currentDriver.FindElement(By.Name("Amount")).SendKeys(data.ToString());
+
+        }
+
+        [When(@"Hago click en Aceptar")]
+        public void WhenHagoClickEnAceptar()
+        {
+            currentDriver.FindElement(By.Name("Aceptar")).Click();
+        }
+
+        [Then(@"Se debe actualizar el saldo")]
+        public void ThenSeDebeMostrarElMensaje()
+        {
+            System.Threading.Thread.Sleep(2000);
+
+            newBalanceData = currentDriver.FindElement(By.Id("dataBalance")).GetAttribute("innerHTML");
+            Assert.AreNotSame(balanceData, newBalanceData);
+            Assert.That(currentDriver.FindElement(By.Id("dataBalance")).Displayed);
+            currentDriver.Quit();
+        }
+    }
+}
---
+++ TestClient/UIAddDeposit.feature
@@ -0,0 +1,11 @@
+﻿Feature: UIAddDeposit
+	Como Usuario Final (humano)
+	Quiero registrar una transaccion de tipo deposito
+
+Scenario: Registro un deposito
+	Given El monto de 10 bolivianos
+	When Navego a la pagina principal
+	And Hago click en el boton Depositar
+	And Lleno el campo del monto a depositar
+	And Hago click en Aceptar
+	Then Se debe actualizar el saldo
